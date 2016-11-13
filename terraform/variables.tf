a84030382142bf48613ef2f59bf1c549ee2e8cd8
Yasser Nabi <yassersaleemi@gmail.com>
2016-11-13 18:55:18 +0000

    Update variables
    Variables updated in preparation for using environment tfvars files and
    splitting environments.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,12 +2,26 @@ data "aws_availability_zones" "available" {}
 
 variable "access_key" {}
 variable "secret_key" {}
-variable "ami_id" { default = "ami-cd87c9be" }
 variable "sshpubkey_file" { default = "sshpub.key" }
-variable "resource_prefix" { default = "yasser_nabi_pressr" }
 variable "region" { default = "eu-west-1" }
-variable "cidr_block" { default = "172.16.0.0/16" }
-variable "primary_subnet" { default = "172.16.19.0/24" }
-variable "secondary_subnet" { default = "172.16.29.0/24" }
+variable "ami_id" {
+  default = "ami-0d77397e"
+}
+variable "instance_type" {
+  default = "t2.small"
+}
+variable "root_disk_size" {
+  default = "25"
+}
 variable "db_username" { default = "pressr" }
 variable "db_password" {}
+# Instance count, per availability zone
+variable "app_instance_count" { default = "2" }
+variable "haproxy_instance_count" { default = "1" }
+variable "consul_instance_count" { default = "2" }
+variable "environment" {}
+variable "resource_prefix" {}
+variable "cidr_block" {}
+variable "primary_subnet" {}
+variable "secondary_subnet" {}
+variable "db_name" {}
